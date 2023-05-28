78956c19f1edad57081222aa130607840aacb625
UgbabeOG <ugbabe.og@gmail.com>
2023-05-28 13:26:47 -0800

    some changes made

---
+++ easybank.js
@@ -0,0 +1,35 @@
+const $ = (x) => document.querySelector(x),
+  $$ = (x) => document.querySelectorAll(x);
+
+const overshadow = $(".overlay");
+const body = $("body");
+const hamburgerIcon = $("#hamburger");
+const hamburgerBtn = $("#hamburger-container");
+const headerMenu = $("#header-menu");
+const homeImage = $(".home-image");
+
+hamburgerBtn.addEventListener("click", () => {
+  if (hamburgerBtn.classList.contains("open")) {
+    body.classList.remove("no-scroll");
+    overshadow.style.display = "none";
+    overshadow.classList.remove("fade-in");
+    overshadow.classList.add("fade-out");
+    headerMenu.classList.add("has-fade");
+    homeImage.style.opacity = "1";
+    homeImage.style.visibility = "visible";
+
+    // todo. create fade-out effect for overshadow
+    // add fade in fade out to header menu
+  } else {
+    body.classList.add("no-scroll");
+    overshadow.style.display = "block";
+    overshadow.classList.add("fade-in");
+    overshadow.classList.remove("fade-out");
+    headerMenu.classList.remove("has-fade");
+    homeImage.style.opacity = "0";
+    homeImage.style.visibility = "hidden";
+  }
+  hamburgerBtn.classList.toggle("open");
+  hamburgerIcon.classList.toggle("fa-xmark");
+  hamburgerIcon.classList.toggle("fa-bars");
+});
